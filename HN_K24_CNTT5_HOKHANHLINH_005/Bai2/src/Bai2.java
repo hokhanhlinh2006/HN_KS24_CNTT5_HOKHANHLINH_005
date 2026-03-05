40ea086eafbe1ef329bed69f4840a67501440b6f
hokhanhlinh2006 <hokhanhlinh2006@gmail.com>
2026-03-05 10:14:54 +0700

    Bai2
    Refactor main method for clarity and functionality.

--- HN_K24_CNTT5_HOKHANHLINH_005/Bai2/src/Bai2.java
+++ HN_K24_CNTT5_HOKHANHLINH_005/Bai2/src/Bai2.java
@@ -1,27 +1,31 @@
 import java.util.*;
 public class Bai2 {
-    public static void main(String[] arge){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap ten danh muc :");
+
+        System.out.print("Nhap danh muc: ");
         String input = sc.nextLine();
-    }
-  if (input.trim().isEmpty()) {
-        System.out.println("Chuoi khong hop le");
-        return;
-    }
+        String[] arr = input.split(",");
 
-    Map<String, Integer> map = new HashMap<>();
-    String[] arr = input.split(",");
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : arr) {
+            s = s.trim().toLowerCase();
+            if (map.containsKey(s)) {
+                map.put(s, map.get(s) + 1);
+            } else {
+                map.put(s, 1);
+            }
+        }
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
 
-        for (int i = 0; i < arr.length; i++) {
-
-        String tag = arr[i].trim().toLowerCase();
-        if (map.containsKey(tag)) {
-            int soLan = map.get(tag);
-            map.put(tag, soLan + 1);
+        System.out.print("Tim kiem: ");
+        String search = sc.nextLine().toLowerCase();
+        if (map.containsKey(search)) {
+            System.out.println("Search \""+search +"\": "+ map.get(search));
         } else {
-            map.put(tag, 1);
+            System.out.println("Search \""+search + "\":0");
         }
     }
-
 }
